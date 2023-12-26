3017c3378158a921ae74acf4130f706e011cbdfe
Jun Astrogal <junastrogal@gmail.com>
2023-12-26 08:15:23 -0500

    init

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+// npx expo install react-native-webview
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      source={{ uri: "https://docs.google.com/forms/d/e/1FAIpQLSfHB7665479MX-Ky_qTAmmz0jds4J_-Wb5Lkl1hiSacaD4ZWA/viewform?usp=sf_link" }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 64,
   },
 });
---
+++ README.md
@@ -0,0 +1,4 @@
+npx create-expo-app ProjectName
+expo-cli start --tunnel
+
+npx expo install react-native-webview
